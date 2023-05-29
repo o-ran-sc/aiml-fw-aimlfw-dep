8ce601e84035622bfa5fe176a1231de6348f3845
josephthaliath <jo.thaliath@samsung.com>
2023-05-29 16:41:26 +0530

    Change to k8s installation based on latest document in k8s main branch
    Issue-Id: AIMLFW-35
    
    Change-Id: I4139fa12863e800d59f744b7b0c66507739923f3
    Signed-off-by: josephthaliath <jo.thaliath@samsung.com>

--- tools/kubernetes/install_k8s.sh
+++ tools/kubernetes/install_k8s.sh
@@ -47,8 +47,8 @@ else
     echo "Found Docker Engine in the system. Skip Docker Engine installation."
 fi
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet=1.21.0-00 kubectl=1.21.0-00 kubeadm=1.21.0-00
 sudo apt-mark hold kubelet kubeadm kubectl
