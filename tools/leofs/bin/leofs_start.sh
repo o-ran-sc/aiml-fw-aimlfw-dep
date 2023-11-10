888553562be7169297a57d5b9c6cacab530f1158
josephthaliath <jo.thaliath@samsung.com>
2023-11-10 14:42:52 +0530

    Changes to handle aimlfw hosted vm restart
    Issue-Id: AIMLFW-63
    
    Change-Id: I03d4890a855de1ed86e1fbadf80194180437ef9e
    Signed-off-by: josephthaliath <jo.thaliath@samsung.com>

--- tools/leofs/bin/leofs_start.sh
+++ tools/leofs/bin/leofs_start.sh
@@ -16,11 +16,29 @@
 #
 # ==================================================================================
 
-dpkg-deb -xv leofs_1.4.3-1_ubuntu-18.04_amd64.deb . 
-sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_manager_0/etc/leo_manager.environment
-sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_manager_1/etc/leo_manager.environment
-sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_storage/etc/leo_storage.environment
-sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_gateway/etc/leo_gateway.environment
+while ! test -d  /proc/1 ; do
+        echo "PID 1 not started"
+        sleep 1
+done
+
+cd ../leofs
+
+
+if test -f leofs_started; then
+  echo "Leofs already installed"
+else
+    echo "Leofs not started"
+    cp ../deb/leofs_1.4.3-1_ubuntu-18.04_amd64.deb .
+    dpkg-deb -xv leofs_1.4.3-1_ubuntu-18.04_amd64.deb . 
+    sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_manager_0/etc/leo_manager.environment
+    sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_manager_1/etc/leo_manager.environment
+    sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_storage/etc/leo_storage.environment
+    sed -i 's/RUNNER_USER=/RUNNER_USER=root/g' ./usr/local/leofs/1.4.3/leo_gateway/etc/leo_gateway.environment
+
+    touch leofs_started
+fi
+
+
 ./usr/local/leofs/1.4.3/leo_manager_0/bin/leo_manager start
 echo "leo_manager_0 started"
 sleep 20
